880c31c43c58dbcd7b55a017a62140a65413649b
Mathew Dony <49617008+mdmathewdc@users.noreply.github.com>
2025-09-07 22:18:37 +1000

    Update Hero.jsx

--- mathew-dony-portfolio/components/Hero.jsx
+++ mathew-dony-portfolio/components/Hero.jsx
@@ -15,11 +15,6 @@ const Hero = () => {
     }, 1500);
   }, []);
 
-  const handleScroll = () => {
-    const resumeSection = document.querySelector("#resume-section");
-    resumeSection.scrollIntoView({ behavior: "smooth", block: "start" });
-  };
-
   return (
     <HeroContainer>
       {isLoading ? (
@@ -46,12 +41,6 @@ const Hero = () => {
               </a>
             </LogosContainer>
           </section>
-          <div className="scroll-down">
-            {/* Add an Href here to scroll to next section */}
-            <a onClick={handleScroll}>
-              <span></span>
-            </a>
-          </div>
         </Fade>
       )}
     </HeroContainer>
@@ -62,55 +51,6 @@ const HeroContainer = styled.div`
   min-height: 100vh;
   color: #fff;
   font-size: 1.3rem;
-
-  .scroll-down a span {
-    position: absolute;
-    top: 80vh;
-    left: 50%;
-    width: 24px;
-    height: 24px;
-    margin-left: -12px;
-    border-left: 2.5px solid;
-    border-bottom: 2.5px solid;
-    -webkit-transform: rotate(-45deg);
-    transform: rotate(-45deg);
-    -webkit-animation: scrolldown 2.75s infinite;
-    animation: scrolldown 2.75s infinite;
-    box-sizing: border-box;
-    border-image: linear-gradient(140deg, #f8081d 0%, #4795c1 100%);
-    border-image-slice: 1;
-  }
-
-  a {
-    cursor: pointer;
-  }
-
-  @-webkit-keyframes scrolldown {
-    0% {
-      -webkit-transform: rotate(-45deg) translate(0, 0);
-      opacity: 0;
-    }
-    50% {
-      opacity: 1;
-    }
-    100% {
-      -webkit-transform: rotate(-45deg) translate(-20px, 20px);
-      opacity: 0;
-    }
-  }
-  @keyframes scrolldown {
-    0% {
-      transform: rotate(-45deg) translate(0, 0);
-      opacity: 0;
-    }
-    50% {
-      opacity: 1;
-    }
-    100% {
-      transform: rotate(-45deg) translate(-20px, 20px);
-      opacity: 0;
-    }
-  }
 
   section {
     padding-top: 25vh;
