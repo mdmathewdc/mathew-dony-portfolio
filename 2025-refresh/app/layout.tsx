47f8ea0d4b0678d507b09c444587f6d7a3b202c2
Mathew Dony <mathewdony007@gmail.com>
2025-11-15 16:32:32 +1100

    Add @vercel/analytics dependency and integrate Analytics component: Update package.json and package-lock.json to include @vercel/analytics, and add the Analytics component to the RootLayout for enhanced user insights.

--- 2025-refresh/app/layout.tsx
+++ 2025-refresh/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const satoshiLight = localFont({
   src: "../fonts/Satoshi-Light.woff2",
@@ -52,6 +53,7 @@ export default function RootLayout({
         className={`${satoshiLight.variable} ${satoshiRegular.variable} ${instrumentSerif.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
