4ccdc8ec52b30f0a632ebe1dd64feee194befd85
Mathew Dony <mathewdony007@gmail.com>
2025-11-15 23:32:10 +1100

    Add @vercel/speed-insights package: Integrate SpeedInsights component into layout for performance monitoring and update package dependencies in package.json and package-lock.json.

--- 2025-refresh/app/layout.tsx
+++ 2025-refresh/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const satoshiLight = localFont({
   src: "../fonts/Satoshi-Light.woff2",
@@ -54,6 +55,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
